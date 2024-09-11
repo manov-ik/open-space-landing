d7abaeb3b1352a9a6e6c5fe2ee9a73544d06010d
Mano K <manozart5@gmail.com>
2024-09-11 21:00:47 +0530

    Initial commit

---
+++ open-space-landing/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import navBar from "./components/navbar";
+
+function App() {
+  return (
+    <>
+      <navBar />
+      <div className="flex">
+        <p className="text-3xl m-auto">fgdfgsdfgs </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ open-space-landing/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function navBar() {
+  return (
+    <>
+      <div className="flex">
+        <div>
+          <p className="text-black text-3xl">sdfakjdh</p>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export default navBar;
