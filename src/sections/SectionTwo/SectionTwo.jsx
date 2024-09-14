4064c708923661350d3b6be9b461d86b4fc5c74a
Alien501 <cvignesh404@gmail.com>
2024-09-14 19:09:24 +0530

    made it responsive

--- src/sections/SectionTwo/SectionTwo.jsx
+++ src/sections/SectionTwo/SectionTwo.jsx
@@ -9,7 +9,7 @@ const SectionTwo = () => {
       placeContent: "center",
     }} className='my-4 p-3'>
       <div className="sec-2-text flex justify-center items-center text-white">
-        <p className="w-[70%] text-xl font-thin">
+        <p className="w-[70%] text-sm font-thin">
         Our mission is to kindle the passion of college students to unite, innovate, and make a real impact! We are dedicated to creating a buzzing space where collaboration sparks new ideas and contributions to open-source projects lead to game-changing solutions. This platform fuels creativity, encourage continuous learning, and support community-driven development. Let's join forces, push boundaries, and turn great ideas into incredible realities!
         </p>
       </div>
